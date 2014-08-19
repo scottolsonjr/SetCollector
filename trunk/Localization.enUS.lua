e3516557a45cc120d1b8d28b5087fbafa0a5d933
sanpedro009 <sanpedro009@c69e8f48-6a53-48a7-a268-3c1e41e30b1b>
2014-08-19 02:41:39 +0000

    Initial commit.

---
+++ trunk/Localization.enUS.lua
@@ -0,0 +1,12 @@
+SetCollectorLocalization = {}
+
+SetCollectorLocalization["ADDON_NAME"] = "Set Collector"
+SetCollectorLocalization["MINIMAP_TOOLTIP"] = "Click to view your gear set progress."
+
+
+
+
+SetCollectorLocalization["TUTORIAL_1"] = "Filter collections and sets by specialization.";
+SetCollectorLocalization["TUTORIAL_2"] = "A preview of the set will\nappear here. Click on a set to the right to preview it here.";
+SetCollectorLocalization["TUTORIAL_3"] = "A list of available gear collections and sets is displayed here. Click on a set\nto preview. It will also show your progress toward completing the set.";
+SetCollectorLocalization["TUTORIAL_4"] = "Frame controls, such as close and character preview reset. Does this need a tooltip?";
