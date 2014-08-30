2bb687261b497bafa7036507ef2e422050292510
lomiedo <lomiedo@c69e8f48-6a53-48a7-a268-3c1e41e30b1b>
2014-08-30 22:14:32 +0000

    Updates filter on specialization change Fixes over sized text display issues in set list Improved localization Added Dungeon Sets

--- Localization.enUS.lua
+++ Localization.enUS.lua
@@ -4,9 +4,10 @@ SetCollectorLocalization["ADDON_NAME"] = "Set Collector"
 SetCollectorLocalization["MINIMAP_TOOLTIP"] = "Click to view your gear set progress."
 
 
-
-
 SetCollectorLocalization["TUTORIAL_1"] = "Filter collections and sets by specialization.";
 SetCollectorLocalization["TUTORIAL_2"] = "A preview of the set will\nappear here. Click on a set to the right to preview it here.";
 SetCollectorLocalization["TUTORIAL_3"] = "A list of available gear collections and sets is displayed here. Click on a set\nto preview. It will also show your progress toward completing the set.";
 SetCollectorLocalization["TUTORIAL_4"] = "Frame controls, such as close and character preview reset. Does this need a tooltip?";
+
+
+SetCollectorLocalization["VOID_STORAGE_NOT_READY"] = "Set Collector: Void Storage not ready to scan. Please close Void Storage and reopen."
