f0e21fd808a6bbc045892f7f2ead7ae11502d0d4
lomiedo <lomiedo@c69e8f48-6a53-48a7-a268-3c1e41e30b1b>
2016-08-20 22:44:49 +0000

    Adds additional sets to the database.

--- Localization.enUS.lua
+++ Localization.enUS.lua
@@ -150,6 +150,8 @@ L["DH_TR_19"]							= "[19] Vestment of Second Sight"
 		L["DK_TR_18"] 							= "[18] Demongaze Armor"
 L["DK_TR_19"] 							= "[19] Dreadwyrm Battleplate"
 L["DK_OTH_19"] 							= "Deathlord's Battleplate"
+L["DK_LG_TANK_110"]				= "Level 110 Legendary Items"
+L["DK_LG_MELEE_110"]					= "Level 110 Legendary Items"
 		
 		
 		L["DR_CD_90"] 							= "The Cycle Set"
